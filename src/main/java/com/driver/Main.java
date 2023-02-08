97f2dedd2efc56502aaff8e46b5698f41d75d4c4
vaibhav <ankitkumar35546567@gmail.com>
2023-02-08 09:52:52 +0530

    correction2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,16 +2,22 @@ package com.driver;
 
 public class Main {
     public static class Product{
-        public int product(int x,int y){}
-        public int product(int x,int y, int z){}
-        public double product(double x, double y){}
+        public int product(int x,int y){
+            return x+y;
+        }
+        public int product(int x,int y, int z){
+            return x+y+z;
+        }
+        public double product(double x, double y){
+            return x+y;
+        }
 
     }
 
     public static void main(String[] args){
         Product p = new Product();
-        p.product();
-        p.product();
-        p.product();
+        p.product(2,4);
+        p.product(1,5,3);
+        p.product(2.1,3.5);
     }
 }
